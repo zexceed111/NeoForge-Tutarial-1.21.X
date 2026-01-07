4971cf8a25db9dd020d2967ce832c824bc8c5c58
Kazurov Kostya <kazurovzxc@mail.ru>
2026-01-07 11:07:42 +0200

    add creative tab busmuth

---
+++ src/main/java/net/kaz/tutorialmod/item/ModCreativeModTabs.java
@@ -0,0 +1,41 @@
+package net.kaz.tutorialmod.item;
+
+import net.kaz.tutorialmod.TutorialMod;
+import net.kaz.tutorialmod.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> BISMUTH_ITEM_TAB = CREATIVE_MODE_TAB.register("bismuth_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.BISMUTH.get()))
+                    .title(Component.translatable("creativetab.tutorialmod.bismuth_items"))
+                    .displayItems((ItemDisplayParameters, output) -> {
+                        output.accept(ModItems.BISMUTH);
+                        output.accept(ModItems.RAW_BISMUTH);
+                    }).build());
+
+    public static final Supplier<CreativeModeTab> BISMUTH_BLOCK_TAB = CREATIVE_MODE_TAB.register("bismuth_blocks_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.BISMUTH_BLOCK.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, "bismuth_items_tab"))
+                    .title(Component.translatable("creativetab.tutorialmod.bismuth_blocks"))
+                    .displayItems((ItemDisplayParameters, output) -> {
+                        output.accept(ModBlocks.BISMUTH_BLOCK);
+                        output.accept(ModBlocks.BISMUTH_ORE);
+                    }).build());
+
+    public static void register (IEventBus eventBus){
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+
+
+}
