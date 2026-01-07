fc9b41d04696c9a0e0132cd3b11928f8d1589fb5
Kazurov Kostya <kazurovzxc@mail.ru>
2026-01-07 14:50:37 +0200

    add loot ore

--- src/main/java/net/kaz/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/kaz/tutorialmod/block/ModBlocks.java
@@ -25,11 +25,10 @@ public class ModBlocks {
                 .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
 
     public static final DeferredBlock<Block> BISMUTH_ORE = registerBlock("bismuth_ore",
-    () -> new DropExperienceBlock(UniformInt.of(2,4),
-        BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
-
+            () -> new DropExperienceBlock(UniformInt.of(2, 4),
+                    BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
     public static final DeferredBlock<Block> BISMUTH_DEEPSLATE_ORE = registerBlock("bismuth_deepslate_ore",
-            () -> new DropExperienceBlock(UniformInt.of(3,6),
+            () -> new DropExperienceBlock(UniformInt.of(3, 6),
                     BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
     private static <T extends  Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block){
