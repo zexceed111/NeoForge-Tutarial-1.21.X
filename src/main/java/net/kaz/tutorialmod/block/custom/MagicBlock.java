eb82bf64c0b83efbab3433863cfa3fe31f809a5c
Kazurov Kostya <kazurovzxc@mail.ru>
2026-01-08 15:06:11 +0200

    add custom tags

--- src/main/java/net/kaz/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/net/kaz/tutorialmod/block/custom/MagicBlock.java
@@ -1,6 +1,7 @@
 package net.kaz.tutorialmod.block.custom;
 
 import net.kaz.tutorialmod.item.ModItems;
+import net.kaz.tutorialmod.util.ModTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvent;
@@ -38,7 +39,7 @@ public class MagicBlock extends Block {
     public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
 
         if(entity instanceof ItemEntity itemEntity){
-            if(itemEntity.getItem().getItem() == ModItems.RAW_BISMUTH.get()){
+            if(isValidItem(itemEntity.getItem())){
                 itemEntity.setItem(new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
             }
 
@@ -48,6 +49,10 @@ public class MagicBlock extends Block {
         }
 
         super.stepOn(level, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack item) {
+        return  item.is(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
